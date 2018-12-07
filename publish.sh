544dae89b96ddee027954000ff793e7b245856aa
Tom Burns <tom@burns.ca>
2018-12-07 10:16:27 -0500

    settle on dir structure

--- publish.sh
+++ publish.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "<modules>" > modules.xml
-cat modules/*.xml >> modules.xml
-echo "</modules>" >> modules.xml
-cp modules.xml ~/Code/Swift/disting/disting/data
+rm -rf ~/Code/Swift/disting/disting/data
+cp -r data ~/Code/Swift/disting/disting
+
