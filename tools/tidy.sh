9ae7deb85bcc2f91ef49383e48a9d9cb0b6ab7cc
Tom Burns <tom@burns.ca>
2018-12-15 10:03:26 -0500

    xml lint

--- tools/tidy.sh
+++ tools/tidy.sh
@@ -4,3 +4,9 @@ for i in data/*/*.html; do
   tidy -i -o $i $i
 done
 
+xmllint --output data/modules.xml --format data/modules.xml
+
+for i in data/*/*.xml; do
+  xmllint --output $i --format $i
+done
+
