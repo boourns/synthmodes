3b22ccd8ca6ea6ebf21005911c4eb509becc1594
Tom Burns <tom@burns.ca>
2018-12-24 08:14:09 -0500

    fix SAM documentation

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -rf ~/Code/Swift/disting/disting/data
-cp -r data ~/Code/Swift/disting/disting
+rm -rf ~/Code/Swift/SynthModes/disting/data
+cp -r data ~/Code/Swift/SynthModes/disting
 
