89640f0aca2902e065418a22588db24e900ae82a
Tom Burns <tom@burns.ca>
2019-01-17 10:46:03 -0500

    remove rings custom css

--- tools/adjust_html.rb
+++ tools/adjust_html.rb
@@ -5,11 +5,7 @@ Dir["./*.html"].each do |filename|
   html = File.open(filename) { |f| Nokogiri::HTML(f) }
   
   html.xpath("//html//body//table").each_with_index do |table, index|
-  	if index == 0
-  	  table["class"] = "parameters bordered"
-  	else
-  	  table["class"] = "bordered"
-  	end
+  	  table["class"] = "parameters"
   end
 
   File.write(filename, html.inner_html)
