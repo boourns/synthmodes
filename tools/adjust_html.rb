62bf7a9fb2a8be74b262d1804cff8347650f232b
Tom Burns <tom@burns.ca>
2019-01-18 10:57:57 -0500

    remove custom styles, now unneeded

--- tools/adjust_html.rb
+++ tools/adjust_html.rb
@@ -4,9 +4,13 @@ require 'byebug'
 Dir["./*.html"].each do |filename|
   html = File.open(filename) { |f| Nokogiri::HTML(f) }
   
-  html.xpath("//html//body//table").each_with_index do |table, index|
-  	  table["class"] = "parameters"
-  end
+  # add class to tables
+  # html.xpath("//html//body//table").each_with_index do |table, index|
+  # 	  table["class"] = "parameters"
+  # end
+
+  # remove custom styles
+  html.xpath('//@style').remove
 
   File.write(filename, html.inner_html)
 end
