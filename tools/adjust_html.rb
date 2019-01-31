ff763620924326ac365128af122a70698ffae087
Tom Burns <tom@burns.ca>
2019-01-30 23:46:02 -0500

    disting mk3 icons

--- tools/adjust_html.rb
+++ tools/adjust_html.rb
@@ -1,20 +1,38 @@
 require "nokogiri"
 require 'byebug'
 
-Dir["./*.html"].each do |filename|
-  html = File.open(filename) { |f| Nokogiri::HTML(f) }
-  
-  # add class to tables
-  # html.xpath("//html//body//table").each_with_index do |table, index|
-  # 	  table["class"] = "parameters"
-  # end
+def adjust_html
+  Dir["./*.html"].each do |filename|
+    html = File.open(filename) { |f| Nokogiri::HTML(f) }
+    
+    # add class to tables
+    # html.xpath("//html//body//table").each_with_index do |table, index|
+    # 	  table["class"] = "parameters"
+    # end
 
-  # remove custom styles
-  #html.xpath('//@style').remove
+    # remove custom styles
+    #html.xpath('//@style').remove
 
-  # update a meta tag
-  html.xpath("//meta[@name='viewport']").each_with_index do |meta, index|
-   	  meta["content"] = "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui"
-   end
-  File.write(filename, html.inner_html)
+    # update a meta tag
+    # html.xpath("//meta[@name='viewport']").each_with_index do |meta, index|
+    #  	  meta["content"] = "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui"
+    #  end
+
+    File.write(filename, html.inner_html)
+  end
 end
+
+def adjust_xml
+  xml = File.open("module.xml") { |f| Nokogiri::XML(f) }
+
+  xml.xpath("//module//sections//section//page").each do |page|
+    if page["name"][1] == "-"
+      ref = page["name"][0..2]
+      page["icon"] = "img/#{ref}.png"
+    end
+  end
+
+  File.write("module.xml", xml.to_s)
+end
+
+adjust_xml
