3e6cc32ef1a8853b0515a22df450bbcd91d3cfce
Tom Burns <tom@burns.ca>
2020-08-05 10:34:49 -0400

    update icons for darkmode support

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -rf ~/Code/Swift/SynthModes/disting/data
-cp -r data ~/Code/Swift/SynthModes/disting
+rm -rf ~/Code/disting/disting/data
+cp -r data ~/Code/disting/disting
 
--- tools/icon_mk4.rb
+++ tools/icon_mk4.rb
@@ -1,11 +1,12 @@
-#letters = %w(a b c d e f g h i j k l m n)
-letters = %w(n)
+letters = %w(a b c d e f g h i j k l m n)
+#letters = %w(n)
 numbers = (1..8).to_a
 
 letters.each do |l|
 	numbers.each do |n|
 		filename = "#{l}#{n}.png"
 		text = "#{l.upcase}#{n}"
-    `convert -background white -fill "#666666"  -font 5by7.regular.ttf -size 90x80 -pointsize 70 -gravity center label:#{text} ../data/disting_mk4/img/#{filename}`
+        cmd = "convert -background transparent -fill \"#a7a7a7\"  -font 5by7.regular.ttf -size 90x80 -pointsize 70 -gravity center label:#{text} ../data/disting_mk4/img/#{filename}"
+        `#{cmd}`
   end
 end
