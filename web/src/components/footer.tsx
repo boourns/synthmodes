e9d67692896607433f5d0bddfe1b97bccbccbd94
Tom Burns <tom@burns.ca>
2023-05-17 09:52:56 -0400

    finish most of footer

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -6,10 +6,19 @@ export type FooterProps = {
 
 export class Footer extends Component<FooterProps, any> {
     render() {
-        return <div style="background-color: black;" className="synthmodes-footer">
-            <div style="background-color:red;" className="synthmodes-footer-col">Col 1</div>
-            <div style="background-color:green;" className="synthmodes-footer-col">Col 2</div>
-            <div style="background-color:blue;" className="synthmodes-footer-col">Col 3</div>
+        return <div className="synthmodes-footer">
+            <div style="background-color:red;" className="synthmodes-footer-col">
+                
+            </div>
+            <div className="synthmodes-footer-col">
+                By <b>tom@burns.ca</b>
+            </div>
+            <div className="synthmodes-footer-col">
+                <a href="https://github.com/boourns">Github</a>
+                <a href="https://instagram.com/gravitronic">Instagram</a>
+                <a href="https://twitter.com/burnsAudio">Twitter</a>
+                <a href="https://sequencer.party">Sequencer Party</a>
+            </div>
         </div>
     }
 }
